053e1a705b905935244140b0973b1258e1ccf14a
Carlos M <carlosedm@protonmail.com>
2021-08-27 17:34:19 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Clock />
+  <Input />
+  <Timer />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Clock from "./components/Clock.vue";
+import Timer from "./components/Timer.vue";
+import Input from "./components/Input.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Clock,
+    Timer,
+    Input,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>The time is {{ hours }} : {{ minutes }}</h2>
+</template>
+
+<script>
+export default {
+  name: "Clock",
+  data() {
+    return {
+      hours: 0,
+      minutes: 0,
+    };
+  },
+  created() {
+    setInterval(() => {
+      var date = new Date();
+      this.hours = date.getHours();
+      this.minutes = date.getMinutes();
+    }, 1000);
+  },
+};
+</script>
