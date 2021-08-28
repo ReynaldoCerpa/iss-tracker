95888733ef711c91488386f9656ba911193ad808
Reynaldo Cerpa <reynaldocerpa07@gmail.com>
2021-08-27 18:40:04 -0700

    Add files via upload

---
+++ src/components/Clock.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>ISS Position: Latitude: {{ pos.iss_position.latitude }} longitude: {{ pos.iss_position.longitude }}</h2>
+</template>
+
+<script>
+export default {
+  name: "Clock",
+  data() {
+    return {
+      pos: 0,
+    };
+  },
+  created() {
+    setInterval(async () => {
+      // var date = new Date();
+      // this.hours = date.getHours();
+      // this.minutes = date.getMinutes();
+      const response = await fetch("http://api.open-notify.org/iss-now.json");
+      this.pos = await response.json();
+      this.totalVuePackages = this.pos.total;
+    }, 1000);
+  },
+};
+</script>
